4e95ed9af706659b9159ce126cccad82e9d33342
Vinay Kumar <110723566+vink08@users.noreply.github.com>
2025-01-18 20:04:01 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,27 +1,10 @@
-// const mysql = require('mysql2');
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Iitjee@2021',
-//     database: 'hacker_news',
-// });
 
-// connection.connect((err) => {
-//     if (err) {
-//         console.error('Error connecting to MySQL:', err);
-//         process.exit(1);
-//     } else {
-//         console.log('Connected to MySQL at', new Date());
-//     }
-// });
-
-// module.exports = connection;
 const mysql = require('mysql2/promise'); // Using promise-based version
 
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Iitjee@2021',
+    password: 'B202507',
     database: 'hacker_news',
     waitForConnections: true,
     connectionLimit: 10
