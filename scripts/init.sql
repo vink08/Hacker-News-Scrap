1113bb0af2890777d7e6b11591d32b4115705298
Vinay Kumar <110723566+vink08@users.noreply.github.com>
2025-01-18 20:07:40 +0530

    Changes

--- scripts/init.sql
+++ scripts/init.sql
@@ -3,7 +3,8 @@ USE hacker_news;
 
 CREATE TABLE IF NOT EXISTS stories (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL UNIQUE,
-    url VARCHAR(255) NOT NULL,
-    posted_at DATETIME NOT NULL
+    title VARCHAR(255) NOT NULL,
+    url VARCHAR(1024) NOT NULL,
+    posted_at DATETIME NOT NULL,
+    UNIQUE KEY unique_story (title(100), url(255))
 );
